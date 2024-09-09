bbf9777bb026624c7e9fb7fe2a7476333da61e8f
akhilrthambhi <142496960+akhilrthambhi@users.noreply.github.com>
2024-09-10 02:42:04 +0530

    script.js

--- script.js
+++ script.js
@@ -19,49 +19,15 @@
 //     }
 // }
 
-
-var user = document.getElementById('us');
-var pass = document.getElementById('pa');
-var userr = document.getElementById('err1');
-var paerr = document.getElementById('err2');
-
-function validate(callback) {
+function handleLogin() {
+    var username = document.getElementById('name').value;
+    var password = document.getElementById('pass').value;
     
-    let isValid = true;
-    
-    if (user.value== '' || pass.value== '') {
-        alert("Please enter the username and password to login");
-        isValid = false; // Set isValid to false if fields are empty
-    }
-    
-    if (user.value == 'admin') {
-        userr.innerText = "Username is valid";
-        userr.style.color = 'green';
+    if (username === 'admin' && password === '12345') {
+        window.location.href = 'todo.html'; 
     } else {
-        userr.innerText = "Username is not valid";
-        userr.style.color = 'red';
-        isValid = false; // Set isValid to false if username is incorrect
+        document.getElementById('message').innerText = 'Invalid username or password';
     }
-    if (pass.value == 12345) {
-        paerr.innerText = "Password is valid";
-        paerr.style.color = 'green';
-    } else {
-        paerr.innerText = "Password is not valid";
-        paerr.style.color = 'red';
-        isValid = false; // Set isValid to false if password is incorrect
-    }
-    
-    if (isValid) {
-        callback(); 
-    }
-    return false; 
 }
 
-function mainpage() {
-    window.location.href = '/ToDoList/mainpage.html'; 
-}
-
-document.querySelector('form').onsubmit = function(event) {
-    event.preventDefault(); // Prevent default form submission
-    validate(mainpage); 
-};
+document.getElementById('log').addEventListener('click', handleLogin);
